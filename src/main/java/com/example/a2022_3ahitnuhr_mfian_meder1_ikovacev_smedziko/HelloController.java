7bb27f708944b29aa4d77a9a90b9380bfaa57f8c
Manuel Eder <83589343+ManuelEderr@users.noreply.github.com>
2022-03-24 14:09:20 +0100

    3 Scenes mit Switch Buttons

--- src/main/java/com/example/a2022_3ahitnuhr_mfian_meder1_ikovacev_smedziko/HelloController.java
+++ src/main/java/com/example/a2022_3ahitnuhr_mfian_meder1_ikovacev_smedziko/HelloController.java
@@ -1,16 +1,64 @@
 package com.example.a2022_3ahitnuhr_mfian_meder1_ikovacev_smedziko;
 
 import Model.Uhr;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class HelloController {
+    public VBox WetterVBox;
+    public HBox UhrzeitHBox;
+    public HBox DatumHBox;
+    public Button DigitaleUhrSwitch;
+    public Button AnalogeUhrSwitch;
+    public Button StoppuhrSwitch;
+    public Button TimerSwitch;
+    public Button StartUhrSwitch;
     @FXML
     private Label welcomeText;
 
+
+
     Uhr uhr = new Uhr();
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Analog.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Digital.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene3(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
     protected void onHelloButtonClick() {
