022b605ff4539bf8cf5f653d336374c9880a08c3
ManuelEderr <meder1@htl-steyr.ac.at>
2022-04-08 09:42:01 +0200

    Logik von Analoge Uhr fertig

--- src/main/java/View/ShowUhrAnalog.java
+++ src/main/java/View/ShowUhrAnalog.java
@@ -3,11 +3,14 @@ package View;
 import Model.Uhr;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class ShowUhrAnalog extends AnchorPane {
@@ -19,20 +22,36 @@ public class ShowUhrAnalog extends AnchorPane {
 
     Line hourLine;
 
-    public ShowUhrAnalog(Uhr uhr, Circle c, Line minutesLine, Line hourLine) {
+    Line secondLine;
+
+    StackPane sp;
+
+
+
+    public ShowUhrAnalog(Uhr uhr, Circle c, Line minutesLine, Line hourLine, Line secondLine, StackPane sp) {
         this.uhr = uhr;
         this.clockCircle = c;
         this.minutesLine = minutesLine;
         this.hourLine = hourLine;
+        this.secondLine = secondLine;
+        this.sp = sp;
     }
 
-    public void showUhr(int hour, int minute) throws IOException {
+    public void showUhr() throws IOException {
+
+        long millis = uhr.getCurrentTime().getTime();
+        Calendar ca = new GregorianCalendar();
+        ca.setTimeInMillis(millis);
+
+        int hour = ca.get(Calendar.HOUR);
+        int minute = ca.get(Calendar.MINUTE);
+        int second = ca.get(Calendar.SECOND);
 
 
+        double clockRadius = clockCircle.getRadius();
+        double centerX = 150;
+        double centerY = 111;
 
-        double clockRadius = 250 * 0.8 * 0.5;
-        double centerX = 50;
-        double centerY = 50;
 
         /*
         //Minutenzeiger
@@ -55,25 +74,42 @@ public class ShowUhrAnalog extends AnchorPane {
         hourLine.setStroke(Color.GREEN);
 
          */
+
+        // Draw second hand
+        double sLength = (clockRadius * 1.70);
+        double xSecond = centerX + sLength
+                * Math.sin(second * (2 * Math.PI / 60));
+        double secondY = centerY - sLength
+                * Math.cos(second * (2 * Math.PI / 60));
+        secondLine = new Line(centerX, centerY, xSecond, secondY);
+        secondLine.setStroke(Color.BLACK);
+        secondLine.setStrokeWidth(5);
+
         // Draw minute hand
-        double mLength = clockRadius * 0.65;
+        double mLength = (clockRadius * 1.95);
         double xMinute = centerX + mLength
                 * Math.sin(minute * (2 * Math.PI / 60));
         double minuteY = centerY - mLength
                 * Math.cos(minute * (2 * Math.PI / 60));
-        Line mLine = new Line(centerX, centerY, xMinute, minuteY);
-        mLine.setStroke(Color.BLUE);
+        minutesLine = new Line(centerX, centerY, xMinute, minuteY);
+        minutesLine.setStroke(Color.BLUE);
+        minutesLine.setStrokeWidth(5);
+
+        minutesLine.getCursor();
+
 
         // Draw hour hand
-        double hLength = clockRadius * 0.5;
-        double hourX = centerX + hLength
-                * Math.sin((hour % 12 + minute / 60.0) * (2 * Math.PI / 12));
-        double hourY = centerY - hLength
-                * Math.cos((hour % 12 + minute / 60.0) * (2 * Math.PI / 12));
-        Line hLine = new Line(centerX, centerY, hourX, hourY);
-        hLine.setStroke(Color.GREEN);
+        double hLength = clockRadius * 1.5;
+        double a = (hour % 12 + minute / 60.0) * (2 * Math.PI / 12);
+        double hourX = centerX + hLength * Math.sin(a);
+        double hourY = centerY - hLength * Math.cos(a);
+        hourLine = new Line(centerX, centerY, hourX, hourY);
+        hourLine.setStroke(Color.GREEN);
+        hourLine.setStrokeWidth(5);
 
-        getChildren().clear();
-        getChildren().addAll(mLine, hLine);
+
+
+        sp.getChildren().clear();
+        sp.getChildren().addAll(minutesLine, hourLine, secondLine, clockCircle);
     }
 }
--- src/main/java/com/example/a2022_3ahitnuhr_mfian_meder1_ikovacev_smedziko/ContentA.java
+++ src/main/java/com/example/a2022_3ahitnuhr_mfian_meder1_ikovacev_smedziko/ContentA.java
@@ -6,6 +6,7 @@ import View.ShowUhrDigital;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -14,12 +15,17 @@ import java.io.IOException;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
 
 /**
  * Controller für Analoge Uhr
  */
 public class ContentA extends AnchorPane {
+    @FXML
+    private StackPane stackPaneAnalog;
 
     @FXML
     private Circle clockCircle;
@@ -30,23 +36,30 @@ public class ContentA extends AnchorPane {
     @FXML
     private Line minutesLine;
 
+    @FXML
+    private Line secondLine;
+
 
     public ContentA() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ContentA.fxml"));
 
         fxmlLoader.setRoot(this);
-        //fxmlLoader.setController(this);
+        fxmlLoader.setController(this);
 
-        Uhr uhr = new Uhr("Leonstein");//nur zum testen, später entfernen
-
-        ShowUhrAnalog a = new ShowUhrAnalog(uhr, clockCircle, minutesLine, hourLine);
-        a.showUhr(10, 50);
 
         try {
             fxmlLoader.load();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println(this.getScaleX());
+
+        Uhr uhr = new Uhr("Leonstein");//nur zum testen, später entfernen
+
+        ShowUhrAnalog a = new ShowUhrAnalog(uhr, clockCircle, minutesLine, hourLine, secondLine, stackPaneAnalog);
+
+        a.showUhr();
     }
 }
 
